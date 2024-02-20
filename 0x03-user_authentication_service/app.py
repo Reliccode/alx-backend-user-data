d2689a810bde7fd82e52d6d1ef552818fe2a82b5
Reliccode <bryanmaritim@gmail.com>
2024-02-20 15:51:09 +0300

    implementing endpoint to register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,9 +3,13 @@
 Flask app
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
 
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route("/", methods=['GET'])
@@ -13,6 +17,24 @@ def api():
     message = {"message": "Bienvenue"}
 
     return jsonify(message)
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    # extract email and password from form data
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        # attempt to register user
+        user = AUTH.register_user(email, password)
+        # if reg successful, return json with user info and status code
+        return jsonify({"email": user.email, "message": "user created"}), 200
+
+    except ValueError:
+        # if user already exists, return json indicating email is registered
+        # already
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
