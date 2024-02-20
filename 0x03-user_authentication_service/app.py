3158b083c9c6154beb9047ccbcac6543c2bf3bda
Reliccode <bryanmaritim@gmail.com>
2024-02-20 15:28:02 +0300

    basic flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -10,9 +10,8 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def api():
-    message = {"message": "Bienvenue"}
 
-    return jsonify(message, status=200)
+    return jsonify({"message": "Bienvenue"})
 
 
 if __name__ == "__main__":
