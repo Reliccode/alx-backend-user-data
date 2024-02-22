657f950509e2feb59651b4bd12c6f3b8dc574a2b
Reliccode <bryanmaritim@gmail.com>
2024-02-22 12:17:22 +0300

    update password from reset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -137,7 +137,7 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
 
             # hash the new password
-            hashed_password = _hash_password()
+            hashed_password = _hash_password(password)
 
             # update user's hashed passwd and reset_token field
             self._db.update_user(
